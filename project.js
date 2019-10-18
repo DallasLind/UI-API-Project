3d28ff1d168ece995972639c605970451ad7fc2f
DallasLind <mcgraw.dallas@gmail.com>
2019-10-18 15:47:28 -0400

    Experimenting with what to put in my div and how to do it

--- project.js
+++ project.js
@@ -14,15 +14,19 @@ for (i = 0; i < acc.length; i++) {
 }
 
 const url = ("http://api.openweathermap.org/data/2.5/forecast?zip=20005&cnt=5&APPID=28b02b409b42c315420779b2660708b7")
+const p1 = document.querySelector('.p1')
+
 
 fetch(url)
     .then(res => res.json())
     .then(res => {
-      console.log("success!", res.list[0].weather)
-      let weather = res.list[0].weather
+      console.log("success!", res.list[0].weather.main)
+      let weather = res.list[0].weather.main
       for (let i =0; i < res.list.length; i++) {
-        console.log(res.list[i].weather)
+        console.log(res.list[i].weather[i].main)
         // .panel.innerText = weather[i]
+        p1.innerText = res.list[i].weather.main
+        div.appendChild(p1)
       }
       })
     .catch(err => console.log("Something went wrong", err));
