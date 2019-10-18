710a54c56b60b8c8a0216cff629654e429060d3c
DallasLind <mcgraw.dallas@gmail.com>
2019-10-18 15:10:49 -0400

    Was able to figure out how to pull weather info, but it's only for one day

--- project.js
+++ project.js
@@ -15,10 +15,16 @@ for (i = 0; i < acc.length; i++) {
 
 const url = ("http://api.openweathermap.org/data/2.5/forecast?zip=20005&cnt=5&APPID=28b02b409b42c315420779b2660708b7")
 
-
 fetch(url)
     .then(res => res.json())
-    .then(res => console.log("success!", res.list))
+    .then(res => {
+      console.log("success!", res.list[0].weather)
+      let weather = res.list[0].weather
+      for (let i =0; i < weather.length; i++) {
+        console.log(weather[i])
+        .panel.innerText = weather[i]
+      }
+      })
     .catch(err => console.log("Something went wrong", err));
 
     
