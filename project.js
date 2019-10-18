5e71a100cd6518d82743605a4a28feda3404119d
DallasLind <mcgraw.dallas@gmail.com>
2019-10-18 16:17:38 -0400

    Possibly playing around with another API, one with more detailed documentation and less workaround

--- project.js
+++ project.js
@@ -13,22 +13,9 @@ for (i = 0; i < acc.length; i++) {
   });
 }
 
-const url = ("http://api.openweathermap.org/data/2.5/forecast?zip=20005&cnt=5&APPID=28b02b409b42c315420779b2660708b7")
-const p1 = document.querySelector('.p1')
+const url = ("https://newsapi.org/v2/sources?apiKey=fa57b51843bc4bfb9e79d6723e7b7e6a")
+const ul = document.querySelector('ul')
 
 
-fetch(url)
-    .then(res => res.json())
-    .then(res => {
-      console.log("success!", res.list[0].weather.main)
-      let weather = res.list[0].weather.main
-      for (let i =0; i < res.list.length; i++) {
-        console.log(res.list[i].weather[i].main)
-        // .panel.innerText = weather[i]
-        p1.innerText = res.list[i].weather.main
-        div.appendChild(p1)
-      }
-      })
-    .catch(err => console.log("Something went wrong", err));
 
     
