5f9418b9980ed73063328a8a0de0ff26f982c2be
DallasLind <mcgraw.dallas@gmail.com>
2019-10-18 14:34:57 -0400

    Started figuring out how to narrow down my API data

--- project.js
+++ project.js
@@ -14,21 +14,11 @@ for (i = 0; i < acc.length; i++) {
 }
 
 const url = ("http://api.openweathermap.org/data/2.5/forecast?zip=20005&cnt=5&APPID=28b02b409b42c315420779b2660708b7")
-const ul = document.querySelector('ul')
 
 
 fetch(url)
     .then(res => res.json())
-    .then(res => console.log("success!", res))
-    .then(function(data){
-      const accordionSlides = document.querySelectorAll(".accordion");
-      data.results.slice(0,5).forEach((accordion, i) => {
-        accordion[i].innerHTML = `
+    .then(res => console.log("success!", res.list))
+    .catch(err => console.log("Something went wrong", err));
 
-
-
-        .catch(err => console.log("Something went wrong", err));
-        `
-      })
-    })
-  
+    
