9dc93fc44c0084fcf5db4c690f22f422bd8d7efb
DallasLind <mcgraw.dallas@gmail.com>
2019-10-18 16:34:13 -0400

    Weather why you so difficult

--- project.js
+++ project.js
@@ -13,9 +13,22 @@ for (i = 0; i < acc.length; i++) {
   });
 }
 
-const url = ("https://newsapi.org/v2/sources?apiKey=fa57b51843bc4bfb9e79d6723e7b7e6a")
+const url = ("http://api.openweathermap.org/data/2.5/forecast?zip=20005&cnt=5&APPID=28b02b409b42c315420779b2660708b7")
 const ul = document.querySelector('ul')
 
 
+fetch(url)
+    .then(res => res.json())
+    .then(res => {
+      console.log("success!", res.list[0].weather.main)
+      let weather = res.list[0].weather.main
+      for (let i =0; i < res.list.length; i++) {
+        console.log(res.list[i].weather[i].main)
+        let li = document.createElement('li')
+        li.innerText = res.list[i].weather[i].main
+        ul.appendChild(li)
+      }
+      })
+    .catch(err => console.log("Something went wrong", err));
 
     
