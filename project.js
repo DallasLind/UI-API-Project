db60b748c4111cf60b69d6838279daa0cb0a853f
DallasLind <mcgraw.dallas@gmail.com>
2019-10-20 15:02:59 -0400

    IT WORKSgit add . IT'S HIDEOUS BUT IT WORKS

--- project.js
+++ project.js
@@ -15,16 +15,43 @@ for (i = 0; i < acc.length; i++) {
 }
 
 const url = ("https://cors-anywhere.herokuapp.com/https://api.darksky.net/forecast/3b390504334b941ad91731bb9adf0af9/42.3601,-71.0589,255657600?exclude=[currently],[flags],[minutely],[hourly]")
-const panels = document.querySelector('panel');
-console.log(panels)
+
+const ul = document.querySelector('ul')
+const panel1Div = document.querySelector(".panel1");
+const panel2Div = document.querySelector(".panel2");
+const panel3Div = document.querySelector(".panel3");
+const panel4Div = document.querySelector(".panel4");
+const panel5Div = document.querySelector(".panel5");
+let button = document.querySelectorAll(".button");
 
 // Menu Items for Accordion
 
 fetch(url)
   .then(res => res.json())
   .then(res => {
+      console.log("success!", res.daily.data[0].icon)
+      let summary = document.createElement("P");
+      summary.innerText = res.daily.data[0].icon;
+      panel1Div.appendChild(summary);
 
+      let temperatureLow = document.createElement("P");
+      temperatureLow.innerText = res.daily.data[0].apparentTemperatureLow;
+      panel2Div.appendChild(temperatureLow);
 
-  })
+      let temperatureHigh = document.createElement("P");
+      temperatureHigh.innerText = res.daily.data[0].apparentTemperatureHigh;
+      panel3Div.appendChild(temperatureHigh);
 
+      let precipitation = document.createElement("P");
+      precipitation.innerText = res.daily.data[0].precipAccumulation;
+      panel4Div.appendChild(precipitation);
+
+      let wind = document.createElement("P");
+      wind.innerText = res.daily.data[0].windSpeed;
+      panel5Div.appendChild(wind);
+      })
     
+.catch(err => console.log("err", err))
+
+
+  
